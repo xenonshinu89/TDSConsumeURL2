862c75a5aabfe0299fc36fcd9115c4f835cc38c5
xenonshinu89 <77114696+xenonshinu89@users.noreply.github.com>
2021-03-27 21:03:36 +0530

    Commit 4

--- src/main/java/io/callRestAPI/Security/SecurityConfiguration.java
+++ src/main/java/io/callRestAPI/Security/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http.authorizeRequests()
-		.antMatchers("**/*Foo*").hasAnyRole("ADMIN","USER")
+		.antMatchers("/*Foo*").hasAnyRole("ADMIN","USER")
 		.antMatchers("/").permitAll()
 		.and().formLogin();
 		
